ec00ad948df220fa95aae5d49c9941844cd811d9
arman672 <ahmedarman672@gmail.com>
2022-07-23 22:17:01 +0530

    project

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const route = require('./routes/route');
-const { default: mongoose } = require('mongoose');
+const {mongoose} = require('mongoose');
 const app = express();
 
 app.use(bodyParser.json());
